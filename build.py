d98dc62e1ec9ead5d2d6ec634f912dffae6bcf30
Juan Pérez <juaniyoalm@gmail.com>
2017-05-30 21:22:27 +0200

    probando jenkins_2

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ from pybuilder.core import use_plugin, init, task
 use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.coverage")
-use_plugin("python.flake8")
+#use_plugin("python.flake8")
 use_plugin("python.install_dependencies")
 use_plugin("python.distutils")
 
@@ -13,7 +13,7 @@ def set_properties(project):
 	project.set_property('unittest_module_glob', '*_prueba')
 	project.set_property('coverage_threshold_warn', 80)
 	project.set_property('coverage_break_build', True)
-	project.set_property('flake8_break_build', True)
+	#project.set_property('flake8_break_build', True)
 	
 
 default_task = ['clean', 'analyze', 'publish']
